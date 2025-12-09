f7936529d69e214638451784dfaf375b398bd51a
Aslbek <boyxorozovaslbek198.com>
2025-12-09 15:51:29 +0500

    Add build.sh and production settings for Render

--- discount_app/settings.py
+++ discount_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--bx!w@zd*iizjg#ekd=o2srye_bu8)67(8tk7-8^r3@i@&cyal'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--bx!w@zd*iizjg#ekd=o2srye_bu8)67(8tk7-8^r3@i@&cyal')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
