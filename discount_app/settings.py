798d2f00ae21a2d6258040ab5e730ce94cf7f502
Aslbek <boyxorozovaslbek198.com>
2025-12-15 18:04:57 +0500

    FINAL STATIC FIX

--- discount_app/settings.py
+++ discount_app/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--bx!w@zd*iizjg#ekd=o2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
