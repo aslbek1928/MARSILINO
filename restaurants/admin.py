6c2222ce280af1bef3400117a4d21dc915869c80
Aslbek <boyxorozovaslbek198.com>
2026-02-06 18:49:34 +0500

    booktable final

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Restaurant, RestaurantImage, Cashier, MenuImage
+from .models import Restaurant, RestaurantImage, Cashier, MenuImage, BookTable
 
 class RestaurantImageInline(admin.TabularInline):
     model = RestaurantImage
@@ -20,3 +20,9 @@ class CashierAdmin(admin.ModelAdmin):
     list_display = ('name', 'restaurant', 'phone_number', 'is_active')
     list_filter = ('restaurant', 'is_active')
     search_fields = ('name', 'phone_number')
+
+@admin.register(BookTable)
+class BookTableAdmin(admin.ModelAdmin):
+    list_display = ('BTID', 'restaurant', 'user', 'date', 'time', 'number_of_people')
+    list_filter = ('restaurant', 'date')
+    search_fields = ('user__phone_number', 'restaurant__name')
