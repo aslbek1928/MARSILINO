e6f5fd80b79274073ac80ca8fb8b4c8632014df5
Aslbek <boyxorozovaslbek198.com>
2026-02-26 15:33:54 +0500

    me page

--- api/urls.py
+++ api/urls.py
@@ -9,26 +9,26 @@ from .views import (
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 api_patterns = [
-    path('health', HealthCheckView.as_view(), name='health-check'),
-    path('register', RegisterView.as_view(), name='api-register'),
-    path('registration', RegisterView.as_view()), # Alias
-    path('otp/send', OTPSendView.as_view(), name='otp-send'),
-    path('otp/verify', OTPVerifyView.as_view(), name='otp-verify'),
-    path('signin', TokenObtainPairView.as_view(), name='api-signin'),
-    path('login', TokenObtainPairView.as_view()), # Alias
-    path('token/refresh', TokenRefreshView.as_view(), name='api-token-refresh'),
+    path('health/', HealthCheckView.as_view(), name='health-check'),
+    path('register/', RegisterView.as_view(), name='api-register'),
+    path('registration/', RegisterView.as_view()), # Alias
+    path('otp/send/', OTPSendView.as_view(), name='otp-send'),
+    path('otp/verify/', OTPVerifyView.as_view(), name='otp-verify'),
+    path('signin/', TokenObtainPairView.as_view(), name='api-signin'),
+    path('login/', TokenObtainPairView.as_view()), # Alias
+    path('token/refresh/', TokenRefreshView.as_view(), name='api-token-refresh'),
     
-    path('tags', TagListView.as_view(), name='tag-list'),
-    path('restaurants', RestaurantListView.as_view(), name='restaurant-list'),
-    path('wallet', WalletView.as_view(), name='wallet'),
-    path('wallet/transactions', WalletTransactionListView.as_view(), name='wallet-transactions'),
-    path('wallet/add', WalletAddView.as_view(), name='wallet-add'),
-    path('wallet/transfer', WalletTransferView.as_view(), name='wallet-transfer'),
-    path('receipt/verify', ReceiptVerifyView.as_view(), name='receipt-verify'),
-    path('me', MeView.as_view(), name='me'),
+    path('tags/', TagListView.as_view(), name='tag-list'),
+    path('restaurants/', RestaurantListView.as_view(), name='restaurant-list'),
+    path('wallet/', WalletView.as_view(), name='wallet'),
+    path('wallet/transactions/', WalletTransactionListView.as_view(), name='wallet-transactions'),
+    path('wallet/add/', WalletAddView.as_view(), name='wallet-add'),
+    path('wallet/transfer/', WalletTransferView.as_view(), name='wallet-transfer'),
+    path('receipt/verify/', ReceiptVerifyView.as_view(), name='receipt-verify'),
+    path('me/', MeView.as_view(), name='me'),
     path('me/liked-restaurants/', LikedRestaurantListView.as_view(), name='liked-restaurant-list'),
     path('me/liked-restaurants/<str:restaurant_id>/<str:action>/', LikedRestaurantView.as_view(), name='liked-restaurant'),
-    path('me/device', FCMDeviceView.as_view(), name='device-registration'),
+    path('me/device/', FCMDeviceView.as_view(), name='device-registration'),
 ]
 
 urlpatterns = [
